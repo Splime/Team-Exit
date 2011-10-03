9438affe93b8875f72d09a8edc211ba21d58e55d
Michael Gruar <splime200@gmail.com>
2011-10-03 00:38:10 -0400

    Finally got OOP w/ multiple files working... a cloud drifts downwards. So exciting!

--- cloud.lua
+++ cloud.lua
@@ -1,1 +1,25 @@
---Filler text
+module(..., package.seeall)
+
+function new()
+    local cloudobj = { img = display.newImage("img/detailed_cloud.png"), speed = 1}
+    cloudobj.img.x = display.contentWidth / 2
+    cloudobj.img.y = 0
+    cloudobj.img.xScale = 1
+    cloudobj.img.yScale = 1
+    
+    setmetatable(cloudobj, { __index = Cloud } )
+    --print("creating cloud")
+    
+    --Update Function...
+    function cloudobj:update(event)
+        cloudobj.img.y = cloudobj.img.y + cloudobj.speed
+    end
+    
+    Runtime:addEventListener("enterFrame", cloudobj.update)
+    
+    return cloudobj
+
+end
+
+
+
--- main.lua
+++ main.lua
@@ -1,7 +1,25 @@
 --We require more minerals
 sprite = require "sprite"
 physics = require "physics"
+--Our own "classes" go here
+local Cloud = require("Cloud")
 
---Start physics and such
+module("Main", package.seeall)
+
+--Start physics and other initializations
 physics.start()
 physics.setDrawMode("hybrid")
+local background = display.newRect(0, 0, display.contentWidth, display.contentHeight)
+background:setFillColor(0,100,200)
+system.setIdleTimer(false) --No more screen going to sleep!
+--system.orentation = "landscapeRight"
+
+world_width = 1600
+curr_y = 0 --The screen is a subset of the world, so store where the screen starts
+cloud9 = Cloud:new()
+
+
+
+
+
+--Put our event listeners here!
