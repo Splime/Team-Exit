e99c31a21bfdc249aacb748c34827e9a1fdf8316
Thomas Anesta <tom.anesta@gmail.com>
2011-10-05 17:43:40 -0400

    commit for merge

--- drill.lua
+++ drill.lua
@@ -3,7 +3,7 @@ module(..., package.seeall)
 --defDrill = {x = 0, y = 0, xvcomp = 1, xycomp = 0, speed = 1, img = display.newImage("img/detailed_cloud.png")}
 
 function new(drizzil, disp, mth, x, y, degrees)--x and y are the location of the center, degrees is the 
-    local drillobj = { img = disp.newImage("img/detailed_cloud.png"), speed = 3, x = x, y = y, xvcomp = mth.cos(mth.rad(degrees)), yvcomp = mth.sin(mth.rad(degrees)), speed = 1}
+    local drillobj = { img = disp.newImage("img/detailed_cloud.png"), speed = 3, x = x, y = y, xvcomp = math.cos(math.rad(degrees)), yvcomp = math.sin(math.rad(degrees)), speed = 1}
     drillobj.img.x = x
     drillobj.img.y = y
     drillobj.img.xScale = 1
