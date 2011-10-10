39be8e3d28c652d0de3baa70f151e29e07b8ae71
Thomas Anesta <tom.anesta@gmail.com>
2011-10-09 21:39:35 -0400

    made some changes to try and start level editing

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 --We require more minerals
 sprite = require "sprite"
 physics = require "physics"
+string = require "string"
 --Our own "classes" go here
 local Cloud = require("cloud")
 local Drill = require("drill")
@@ -9,6 +10,40 @@ local Bird = require("bird")
 local Crap = require("crap")
 local Player = require("player")
 
+--level loading related variables
+local maxlevel = 1--the last level in the game
+local startlevel = 0
+local levelkey = {"level", ".txt"}
+local delimiter = "^"
+
+--string splitting function for level reading, attributed to http://lua-users.org/wiki/SplitJoin
+
+function Split(str, delim, maxNb)
+    --print(str)
+    -- Eliminate bad cases...
+    if string.find(str, delim) == nil then
+        return { str }
+    end
+    if maxNb == nil or maxNb < 1 then
+        maxNb = 0    -- No limit
+    end
+    local result = {}
+    local pat = "(.-)" .. delim .. "()"
+    local nb = 0
+    local lastPos
+    for part, pos in string.gfind(str, pat) do
+        nb = nb + 1
+        result[nb] = part
+        lastPos = pos
+        if nb == maxNb then break end
+    end
+    -- Handle the last field
+    if nb ~= maxNb then
+        result[nb + 1] = string.sub(str, lastPos)
+    end
+    return result
+end
+
 --Start physics and other initializations
 physics.start()
 physics.setDrawMode("hybrid")
@@ -16,6 +51,7 @@ physics.setGravity(0, 0)
 local background = display.newRect(0, 0, display.contentWidth, display.contentHeight)
 background:setFillColor(0,100,200)
 system.setIdleTimer(false) --No more screen going to sleep!
+
 
 --sound effects
 sounds = {
@@ -28,24 +64,109 @@ maxDrillDelay = 100
 aimposx = 0
 aimposy = 0
 lastFrameTime = 0
-cloud9 = Cloud:new(0, 10, 1)
 drillCooldown = 0
-balloon = Player:new(200, 200)
 drillList = {}
 rainList = {}
 cloudList = {}
 birdList = {}
 crapList = {}
-table.insert(cloudList, cloud9)
+levelList = {}
 
+cloud9 = Cloud:new(0, 10, 1)
+table.insert(cloudList, cloud9)
 birdtest = Bird:new(display.contentWidth - 100, 10, -1)
 table.insert(birdList, birdtest)
+
+
+balloon = Player:new(200, 200)
 
 --On Accel, deals with accelerator input
 -- local function onAccel(event)
     -- accelSpeed = centerX + (centerX * event.xGravity)
 	-- -- Circle.y = centerY + (centerY * event.yGravity * -1)
 -- end
+
+--loads a level from a text file
+--note that time is in frames each of which is 33 milliseconds, 30 frames a second
+local function loadLevel()
+    --start by clearing the level
+    levelList={}
+    print ("loading a level")
+    startlevel = startlevel + 1
+    if(startlevel > maxlevel) then
+        print ("no more levels to load")
+        return
+    end
+    local pathval = (levelkey[1] .. startlevel .. levelkey[2])
+    print (pathval)
+    local path = system.pathForFile(pathval)--system.ResourceDirectory
+    local file = io.open(path, "r")
+    if not file then
+        print("error loading level, game aborted")
+        --os.exit()
+    else
+        print("file exists")
+        print(file.path)
+    end
+    
+    --first get our level
+    local levelVal = assert (io.read("*l"), "error reading the file")
+    --string stripping operation from http://lua-users.org/wiki/StringTrim
+    --levelVal = levelVal:match'^%s*(.*%S)'
+    print(levelval)
+    local levelDescription = Split(levelVal, delimiter)
+    --set the description of the level
+    levelList[levelDescription[1]] = {levelDescription[2], levelDescription[3]}
+    --establish variable to decide whether or not we are done reading
+    local eof = false
+    local wave = {}
+    local obj = {}
+    wave["time"] = 0
+    local fullLine = ''
+    while not eof do
+        fullLine = assert(io.read("*l"), "error reading the file")
+        if (fullLine ~= nil) then
+            fullLine = Split(fullLine, delimiter)
+            if(fullLine[1] == "ENDWAVE" ) then
+                levelList[wave["time"]] = wave
+            elseif (fullLine[1] == "BEGINWAVE") then
+                wave={}
+                if (fullLine[2] == nil) then
+                    wave["time"] = 0
+                else
+                    wave["time"] = fullLine[2]
+                end--else we have a class to add to the wave
+            elseif (fullLine[1] == "Cloud") then--cloud, x pos, y pos, speed
+                obj={}
+                table.insert(obj, fullLine[1])
+                table.insert(obj, fullLine[2])
+                table.insert(obj, fullLine[3])
+                table.insert(obj, fullLine[4])
+                table.insert(wave, obj)
+            elseif (fullLine[1] == "Bird") then--bird, x pos, y pos, speed
+                obj={}
+                table.insert(obj, fullLine[1])
+                table.insert(obj, fullLine[2])
+                table.insert(obj, fullLine[3])
+                table.insert(obj, fullLine[4])
+                table.insert(wave, obj)
+            else
+                print("error reading file")
+            end
+        else
+            eof = true
+        
+        end
+        
+    end
+    
+    
+    wave={}
+    obj={}
+    file:close()
+    
+
+end
 
 --Update, happens every frame
 local function update(event)
@@ -101,6 +222,28 @@ local function update(event)
     balloon:update(event, accelSpeed)
     --Finished updating? Now change the previous time
     lastFrameTime = event.time
+end
+
+local function populate(event)
+    if (levelList[event.count] == nil) then
+        return--then there is no wave at this time
+    else
+        for index, value in ipairs(levelList[event.count]) do
+            if (value[1] == "Cloud") then
+                local newCloud = Cloud:new(value[2], value[3], value[4])
+                table.insert(birdList, newBird)
+            else if (value[1] == "Bird") then
+                local newBird = Bird:new(value[2], value[3], value[4])
+                table.insert(birdList, newBird)
+            else
+                return
+            
+            then
+        
+        end
+    
+    end
+
 end
 
 
@@ -190,3 +333,10 @@ Runtime:addEventListener("accelerometer", onAccel)
 Runtime:addEventListener("enterFrame", update)
 Runtime:addEventListener("touch", onTouch)
 Runtime:addEventListener("collision", onCollision)
+--start our timer
+--timer.performWithDelay(33, update, 0)
+--timer.performWithDelay(33, populate, 0)
+
+
+--start the actual game
+--loadLevel()
